43654a7f366f41c4fa4dd7573a7fb4b8f0eb211b
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2018-06-02 15:59:34 +0100

    Added blocking for Ice dude

---
+++ Assets/Scripts/Player/Ice/IceMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceMotor : PlayerMotor
+{
+    [SerializeField] private float dodgeDistance = 3.0f;
+
+    public override void Evade()
+    {
+        base.Evade();
+        StartCoroutine(CarryOutEvade());
+    }
+
+    private IEnumerator CarryOutEvade()
+    {
+        controller.allowInput = false;
+
+        velocity = Vector3.zero;
+
+        controller.anim.SetTrigger("evade");
+
+        AnimatorStateInfo stateInfo = controller.anim.GetCurrentAnimatorStateInfo(0);
+        float secs = stateInfo.length;
+
+        while (secs > 0)
+        {
+            secs -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        controller.allowInput = true;
+    }
+}
