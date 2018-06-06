e02fbc51d19adac695f67db57f51813a37d2a9b1
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2018-06-07 00:58:58 +0100

    Started projectile

---
+++ Assets/Scripts/Player/Other/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public static float moveSpeed = 10.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.forward * moveSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<IceMotor>() || other.GetComponent<FireMotor>())
+        {
+            return;
+        }
+
+        iKillable killable = other.GetComponent<iKillable>();
+        if (killable != null)
+        {
+            killable.ApplyDamage(1);
+        }
+    }
+}
---
+++ Assets/Scripts/Testing/DamageableCapsule.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableCapsule : MonoBehaviour, iKillable
+{
+    [SerializeField] private int originalHP = 10;
+    [SerializeField] private Color32 hitColour;
+
+    private Color32 originalColour;
+    private Material mat;
+    private int hitPoints = 10;
+
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        originalColour = mat.color;
+
+        hitPoints = originalHP;
+    }
+
+    void iKillable.ApplyDamage(int damage)
+    {
+        hitPoints -= damage;
+
+        if (hitPoints <= 0)
+        {
+            Debug.Log("DED");
+            Respawn();
+        }
+        else
+        {
+            Debug.Log("Took " + damage + " damage \n" + "HP is now " + hitPoints);
+            StartCoroutine(ColourChange());
+        }
+    }
+
+    IEnumerator ColourChange()
+    {
+        mat.color = hitColour;
+
+        while (mat.color != originalColour)
+        {
+            mat.color = Color32.LerpUnclamped(mat.color, originalColour, Time.deltaTime * 2);
+            yield return null;
+        }
+    }
+
+    void Respawn()
+    {
+        hitPoints = originalHP;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
+}
