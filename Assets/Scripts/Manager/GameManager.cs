a935ac3537cae141ac7b8a7ec9ff1ee20323acdb
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2018-06-06 20:16:42 +0100

    Player cna now be chosen (Not at runtime yet)

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,70 +4,69 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool overridePlayerCount = false;
-    public int playerCount = 2;
+    #region Singleton
+    public static GameManager instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    #endregion
+
+    public int playerCount = 1;
+    public int playerOneChar = 1;
     public RuntimePlatform map = RuntimePlatform.XboxOne;
 
     void Start()
     {
-        if (overridePlayerCount)
-        {
-            SpawnPlayers(playerCount);
-        }
-        else
-        {
-            SpawnPlayers();
-        }
+        // Spawns both players upon starting game.
+        // WHEN THE GAME IS IMPLEMENTED, CHANGE THIS.
+        SpawnPlayers();
     }
+
 
     void SpawnPlayers()
     {
-        Debug.Log(Input.GetJoystickNames());
-        switch (Input.GetJoystickNames().Length)
+        // The number of players is decided on here.
+        switch (playerCount)
         {
             case 1:
-                FindCharacterOnScene(true).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
+                // Finds the chosen guy and sets them as player 1.
+                FindCharacterOnScene(playerOneChar).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
                 break;
 
             case 2:
-                FindCharacterOnScene(true).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
-                FindCharacterOnScene(false).GetComponent<PlayerController>().SetPlayerInfo(map, 2);
+                // Sets the fire guy as player 1 and the ice guy as player 2.
+                FindCharacterOnScene(1).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
+                FindCharacterOnScene(2).GetComponent<PlayerController>().SetPlayerInfo(map, 2);
                 break;
 
             default:
-                FindCharacterOnScene(true).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
+                // Finds the chosen guy and sets them as player 1.
+                FindCharacterOnScene(playerOneChar).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
                 break;
         }
     }
 
-    void SpawnPlayers(int count)
+
+    GameObject FindCharacterOnScene(int character)
     {
-        switch (count)
+        // Switches depending on which character player 1 chose.
+        if (character == 1)
         {
-            case 1:
-                FindCharacterOnScene(true).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
-                break;
-
-            case 2:
-                FindCharacterOnScene(true).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
-                FindCharacterOnScene(false).GetComponent<PlayerController>().SetPlayerInfo(map, 2);
-                break;
-
-            default:
-                FindCharacterOnScene(true).GetComponent<PlayerController>().SetPlayerInfo(map, 1);
-                break;
-        }
-    }
-
-    GameObject FindCharacterOnScene(bool isFire)
-    {
-        if (isFire)
-        {
+            Debug.Log("Found Fire");
             return GameObject.FindObjectOfType<FireMotor>().gameObject;
+        }
+        else if (character == 2)
+        {
+            Debug.Log("Found Ice");
+            return GameObject.FindObjectOfType<IceMotor>().gameObject;
         }
         else
         {
-            return GameObject.FindObjectOfType<IceMotor>().gameObject;
+            return null;
         }
     }
 }
---
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI chosenText;
+    public int charID = 1;
+
+    public void Navigate()
+    {
+        if (charID == 1)
+        {
+            charID = 2;
+            chosenText.text = "Ice";
+        }
+        else
+        {
+            charID = 1;
+            chosenText.text = "Fire";
+        }
+    }
+
+    public void ChooseCharacter()
+    {
+    }
+}
